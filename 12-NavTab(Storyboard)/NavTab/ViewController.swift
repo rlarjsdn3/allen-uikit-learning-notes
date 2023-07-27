7fb9c49af036ac219741a593a26d4afe74fab6f2
Zizi_Kim <rlarjsdn3@naver.com>
2023-07-27 11:26:48 +0900

    NavTab(Storyboard) 프로젝트 추가

---
+++ 12-NavTab(Storyboard)/NavTab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavTab
+//
+//  Created by 김건우 on 7/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
